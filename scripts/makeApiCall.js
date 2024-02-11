df0c857a3973b2f62cdb2fc26eafe13b2c1c230c
Mario5648 <65876827+Mario5648@users.noreply.github.com>
2024-02-11 14:04:49 -0800

    Add files via upload

---
+++ scripts/makeApiCall.js
@@ -0,0 +1,32 @@
+function sendMessage(callBack = null)
+{
+
+    let params = {
+        "organization": "mantisSoftware",
+        "token": "c616bd12db4faa3a28b14cdf510311915b16d9c46117f194d496c0a0edead934",
+        "name" : document.getElementById("clientName").value,
+        "companyName" : document.getElementById("clientCompanyName").value,
+        "email" : document.getElementById("clientEmail").value,
+        "message": document.getElementById("clientMessage").value
+    };
+
+    endpointCall("sendMessage", params, function(data)
+                                {
+                                    //Store id and name in cookies for later use
+                                    if(data["status"] == "success")
+                                    {
+                                        callBack('Successfully sent message!');
+                                    }else if(data["status"] == "failed")
+                                    {
+                                        callBack("Failed to send message. Please try again!");
+                                    }
+                                });
+}
+
+function sendMessageOnClick()
+{
+    sendMessage( function(responseMessage)
+        {
+            alert(responseMessage);
+        });
+}
